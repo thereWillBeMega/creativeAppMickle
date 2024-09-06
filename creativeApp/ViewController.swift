3e76fed82402dcaeb1e9fd4fbb3b472a391a3456
Peter Mickle <robloxpro408@gmail.com>
2024-09-05 21:55:27 -0500

    code yay

--- creativeApp/ViewController.swift
+++ creativeApp/ViewController.swift
@@ -10,12 +10,13 @@ import UIKit
 class ViewController: UIViewController {
     
 
-    var grassScore: Int = 0
+    var grassScore: Double = 0
     
     @IBOutlet weak var gameOutlet: UITextField!
     
     @IBOutlet weak var hoursOutlet: UITextField!
     
+    @IBOutlet weak var imageOutlet: UIImageView!
     
     
     override func viewDidLoad() {
@@ -27,13 +28,60 @@ class ViewController: UIViewController {
     
 
     @IBAction func calculateAction(_ sender: UIButton) {
+    
+        //fixed by my good friend chatGPT
+        if let text = hoursOutlet.text, !text.isEmpty, let hours = Double(text) {
+            
+            if let game = gameOutlet.text{
+                
+                if hours < 0{
+                    imageOutlet.image = UIImage(named: "Invalad")
+                    return
+                }
+                
+                if game == "LoL" || game == "League of Legends"{
+                    grassScore = hours * 4
+                }else if game == "Terraria"{
+                    grassScore = hours * 1.5
+                }else if game == "ADOFAI" || game == "A Dance of Fire and Ice"{
+                    grassScore = hours * 1.1
+                }else if game == "Mario" || game == "Pokemon" || game == "Fortnite" || game == "Roblox"{
+                    grassScore = hours
+                }else if game == "Omori"{
+                    grassScore = hours * 0.9
+                }else if game == "Minecraft"{
+                    grassScore = hours * 0.25
+                }else{
+                    print("invalad1")
+                    imageOutlet.image = UIImage(named: "Invalad")
+                    return
+                }
+            }else{
+                print("invalad2")
+                imageOutlet.image = UIImage(named: "Invalad")
+                return
+            }
+        } else {
+print("invalad3")
+            imageOutlet.image = UIImage(named: "Invalad")
+            return
+        }
         
-        
-        
-//        if(let grass = hoursOutlet.text!){
-//            
-//        }
+        if grassScore < 250{
+           print("yes")
+            imageOutlet.image = UIImage(named: "YesGrass")
+        }else{
+            print("no")
+            imageOutlet.image = UIImage(named: "NoGrass")
+        }
 
+    }
+    
+    
+    @IBAction func ResetAction(_ sender: UIButton) {
+        imageOutlet.image = UIImage(named: "Grass")
+        gameOutlet.text = ""
+        hoursOutlet.text = ""
     }
     
 }
